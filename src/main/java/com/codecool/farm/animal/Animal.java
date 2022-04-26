8a31e64a35fff21dac2bc83780411a950f68cbe0
BiroNora <52859737+BiroNora@users.noreply.github.com>
2022-04-26 11:51:10 +0200

    Tests 16/16

--- src/main/java/com/codecool/farm/animal/Animal.java
+++ src/main/java/com/codecool/farm/animal/Animal.java
@@ -1,4 +1,15 @@
 package com.codecool.farm.animal;
 
-public class Animal {
+public abstract class Animal {
+    private int size;
+
+    public Animal() {
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public abstract void feed();
 }
--- src/main/java/com/codecool/farm/animal/Pig.java
+++ src/main/java/com/codecool/farm/animal/Pig.java
@@ -1,4 +1,14 @@
 package com.codecool.farm.animal;
 
-public class Pig {
+public class Pig extends Animal {
+    private int size;
+    @Override
+    public void feed() {
+        size = getSize() + 1;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
 }
